f93fabb528a31ed05497bd9967637858f14e76aa
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-08-05 17:03:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ app.add_middleware(
 )
 
 # MongoDB connection
-MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
+MONGO_URL = os.environ.get('MONGO_URL')
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.vishal_jwellers
 
